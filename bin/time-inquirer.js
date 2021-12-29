7a846730acdcd4f2e4bd2cf348231ed0b8f84ba5
todd <todd.ruan@thimble.com>
2021-12-29 16:17:15 +0800

    first commit

---
+++ bin/time-inquirer.js
@@ -0,0 +1,38 @@
+// const R = require('ramda');
+//
+// const Day = require('../lib/day');
+const Repl = require('../lib/repl');
+const DB = require('../lib/db');
+const date = moment().format('YYYY-MM-DD');
+// const notify = require('../lib/notify')
+
+// const { NotificationCenter } = require('node-notifier');
+// const nc = new NotificationCenter();
+
+// nc.notify({
+//     title: '1',
+//     message: '2',
+//     sound: 'Funk',
+//     // reply: true,
+// });
+
+Repl.boot();
+// col.insert({a : 1});
+
+
+// Day.show([
+//     {
+//         type: 'work',
+//         startTime: 1,
+//     },
+//     {
+//         type: 'play',
+//         startTime: 50,
+//     },
+//     {
+//         type: 'N/A',
+//         startTime: 100,
+//     }
+// ]);
+
+
